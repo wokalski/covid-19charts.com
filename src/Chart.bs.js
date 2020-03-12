2a40d18078b03798dd9993c1c2db1b25f5f3a74f
Wojtek Czekalski <me@wczekalski.com>
2020-03-12 20:16:34 +0100

    Adjust domain manually

--- src/Chart.bs.js
+++ src/Chart.bs.js
@@ -3,10 +3,24 @@
 var Block = require("bs-platform/lib/js/block.js");
 var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
+var Js_dict = require("bs-platform/lib/js/js_dict.js");
 var Recharts = require("recharts");
 var BsRecharts__XAxis = require("@ahrefs/bs-recharts/src/BsRecharts__XAxis.bs.js");
 var BsRecharts__YAxis = require("@ahrefs/bs-recharts/src/BsRecharts__YAxis.bs.js");
 var BsRecharts__ResponsiveContainer = require("@ahrefs/bs-recharts/src/BsRecharts__ResponsiveContainer.bs.js");
+
+function calculateMaxValue(locations, data) {
+  return data.reduce((function (maxValue, row) {
+                return locations.reduce((function (maxValue, $$location) {
+                              var match = Js_dict.get(row, $$location.label);
+                              if (match !== undefined) {
+                                return Math.max(maxValue, match);
+                              } else {
+                                return maxValue;
+                              }
+                            }), maxValue);
+              }), 1);
+}
 
 function Chart(Props) {
   var data = Props.data;
@@ -15,10 +29,10 @@ function Chart(Props) {
   var scale = Props.scale;
   var threshold = Props.threshold;
   var divRef = React.useRef(null);
-  var domain = scale !== 5395588 ? undefined : /* array */[
-      threshold === 0 ? "dataMin" : threshold,
-      "dataMax"
-    ];
+  var domain = /* array */[
+    threshold === 0 ? 1 : threshold,
+    calculateMaxValue(locations, data)
+  ];
   var match = React.useState((function () {
           return true;
         }));
@@ -28,14 +42,14 @@ function Chart(Props) {
           var opt = divRef.current;
           if (!(opt == null)) {
             Curry._1(setDot, (function (param) {
-                    return opt.clientHeight > 700;
+                    return opt.clientHeight > 500;
                   }));
           }
           return ;
         }), /* array */[]);
   return React.createElement("div", {
               ref: divRef,
-              className: "flex-1"
+              className: "max-h-screen flex-1"
             }, React.createElement(Recharts.ResponsiveContainer, BsRecharts__ResponsiveContainer.makeProps(undefined, undefined, /* Prc */Block.__(1, [100]), 400, undefined, /* Prc */Block.__(1, [100]), React.createElement(Recharts.LineChart, {
                           data: data,
                           margin: {
@@ -57,26 +71,32 @@ function Chart(Props) {
                                           });
                               })), React.createElement(Recharts.Tooltip, {
                               content: (function (data) {
-                                  return React.createElement("div", {
-                                              className: "tooltip flex flex-col border-solid border border-gray-800 rounded p-2"
-                                            }, data.payload.map((function (payload) {
-                                                    return React.createElement("span", {
-                                                                key: payload.dataKey,
-                                                                className: "tooltip-label"
-                                                              }, React.createElement("span", {
-                                                                    style: {
-                                                                      color: payload.stroke
-                                                                    }
-                                                                  }, payload.name), data.separator + payload.value);
-                                                  })));
+                                  var match = data.payload;
+                                  if (match !== null) {
+                                    return React.createElement("div", {
+                                                className: "tooltip flex flex-col border-solid border border-gray-800 rounded p-2"
+                                              }, match.map((function (payload) {
+                                                      return React.createElement("span", {
+                                                                  key: payload.dataKey,
+                                                                  className: "tooltip-label"
+                                                                }, React.createElement("span", {
+                                                                      style: {
+                                                                        color: payload.stroke
+                                                                      }
+                                                                    }, payload.name), data.separator + payload.value);
+                                                    })));
+                                  } else {
+                                    return null;
+                                  }
                                 })
                             }), React.createElement(Recharts.XAxis, BsRecharts__XAxis.makeProps(undefined, undefined, undefined, undefined, undefined, undefined, "name", undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, {
                                   left: 0,
                                   right: 30
-                                }, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, /* () */0)), React.createElement(Recharts.YAxis, BsRecharts__YAxis.makeProps(undefined, undefined, undefined, undefined, undefined, undefined, undefined, domain, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, scale, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, /* () */0)), React.createElement(Recharts.Legend, { })), /* () */0)));
+                                }, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, /* () */0)), React.createElement(Recharts.YAxis, BsRecharts__YAxis.makeProps(/* number */561678025, undefined, undefined, undefined, undefined, undefined, undefined, domain, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, scale, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, /* () */0)), React.createElement(Recharts.Legend, { })), /* () */0)));
 }
 
 var make = Chart;
 
+exports.calculateMaxValue = calculateMaxValue;
 exports.make = make;
 /* react Not a pure module */
--- src/Chart.re
+++ src/Chart.re
@@ -4,36 +4,52 @@ type domain;
 external int_to_domain: int => domain = "%identity";
 external string_to_domain: string => domain = "%identity";
 [@bs.get] external clientHeight: Dom.element => float = "clientHeight";
+external castData: array('a) => array(Js.Dict.t(int)) = "%identity";
+
+let calculateMaxValue = (locations, data) => {
+  Js.Array.reduce(
+    (maxValue, row) => {
+      Js.Array.reduce(
+        (maxValue, location) => {
+          switch (Js.Dict.get(row, location.ReactSelect.label)) {
+          | Some(x) => Js.Math.max_int(maxValue, x)
+          | None => maxValue
+          }
+        },
+        maxValue,
+        locations,
+      )
+    },
+    1,
+    castData(data),
+  );
+};
 
 [@react.component]
 let make = (~data, ~color, ~locations, ~scale, ~threshold) => {
   let divRef = React.useRef(Js.Nullable.null);
   let domain =
-    switch (scale) {
-    | `log =>
-      Some([|
-        if (threshold === 0) {
-          "dataMin" |> string_to_domain;
-        } else {
-          threshold |> int_to_domain;
-        },
-        "dataMax" |> string_to_domain,
-      |])
-    | _ => None
-    };
+    Some([|
+      if (threshold === 0) {
+        1 |> int_to_domain;
+      } else {
+        threshold |> int_to_domain;
+      },
+      calculateMaxValue(locations, data) |> int_to_domain,
+    |]);
   let (dot, setDot) = React.useState(() => true);
   React.useEffect1(
     () => {
       let opt = divRef |> React.Ref.current |> Js.Nullable.toOption;
       switch (opt) {
-      | Some(ref) => setDot(_ => clientHeight(ref) > 700.)
+      | Some(ref) => setDot(_ => clientHeight(ref) > 500.)
       | None => ()
       };
       None;
     },
     [||],
   );
-  <div ref={ReactDOMRe.Ref.domRef(divRef)} className="flex-1">
+  <div ref={ReactDOMRe.Ref.domRef(divRef)} className="max-h-screen flex-1">
     <ResponsiveContainer minHeight=400 height={Prc(100.)} width={Prc(100.)}>
       <LineChart
         margin={"top": 20, "right": 50, "bottom": 20, "left": 0} data>
@@ -52,29 +68,33 @@ let make = (~data, ~color, ~locations, ~scale, ~threshold) => {
          |> React.array}
         <Tooltip
           content={data => {
-            <div
-              className="tooltip flex flex-col border-solid border border-gray-800 rounded p-2">
-              {Js.Array.map(
-                 payload => {
-                   <span className="tooltip-label" key=payload##dataKey>
-                     <span
-                       style={ReactDOMRe.Style.make(
-                         ~color=payload##stroke,
-                         (),
-                       )}>
-                       {React.string(payload##name)}
+            switch (Js.Null.toOption(data##payload)) {
+            | Some(payload) =>
+              <div
+                className="tooltip flex flex-col border-solid border border-gray-800 rounded p-2">
+                {Js.Array.map(
+                   payload => {
+                     <span className="tooltip-label" key=payload##dataKey>
+                       <span
+                         style={ReactDOMRe.Style.make(
+                           ~color=payload##stroke,
+                           (),
+                         )}>
+                         {React.string(payload##name)}
+                       </span>
+                       {React.string(data##separator ++ payload##value)}
                      </span>
-                     {React.string(data##separator ++ payload##value)}
-                   </span>
-                 },
-                 data##payload,
-               )
-               |> React.array}
-            </div>
+                   },
+                   payload,
+                 )
+                 |> React.array}
+              </div>
+            | None => React.null
+            }
           }}
         />
         <XAxis dataKey="name" padding={"left": 0, "right": 30} />
-        <YAxis scale ?domain />
+        <YAxis _type=`number scale ?domain />
         <Legend />
       </LineChart>
     </ResponsiveContainer>
